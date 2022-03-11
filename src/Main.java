6b0d2ffb0fff885a2fe044247101d84e0e5a06d0
AveIvan94 <a.iwanowska@icloud.com>
2022-03-11 10:57:26 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[]args){
+        int tablicaCyfr[] = new int[10];
+        tablicaCyfr = new int[] {1,2,3,4,5,6,7,8,9,10};
+
+        for (int i = 9; i < tablicaCyfr.length; i--) {
+            System.out.println(tablicaCyfr[i]);
+
+        }
+
+
+    }
+}
